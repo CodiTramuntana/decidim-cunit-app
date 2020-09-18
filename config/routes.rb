8605ad4e05785be781a8763171541742be41ebeb
Ivan Molinero <ivan.mr@coditramuntana.com>
2020-09-18 14:43:42 +0200

    Add gem decidim-file_authorization_handler for csv census with id and birtdate data

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  mount Decidim::FileAuthorizationHandler::AdminEngine => '/admin'
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
