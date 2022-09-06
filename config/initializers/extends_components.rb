32789e83aaae3ea498490cd863330193b9d4b736
Laura Jaime <laura.jv@coditramuntana.com>
2022-09-06 10:05:43 +0200

    Add personal data to proposals settings

---
+++ config/initializers/extends_components.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Extends the Decidim::Proposals component.
+
+def add_attribute_to_global_settings(component, attr_name, type, default = false)
+  component.settings(:global).attribute(attr_name.to_sym, type: type, default: default)
+end
+
+component = Decidim.find_component_manifest :proposals
+add_attribute_to_global_settings(component, :personal_data, :boolean, false)
+
