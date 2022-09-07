3d97b64d7f1f33bc10b08530944bb6731db308ee
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-09-07 17:05:16 +0200

    TEST: Resolve dependency from decidim-proposals shared context

--- spec/system/admin/view_proposal_details_from_admin_spec.rb
+++ spec/system/admin/view_proposal_details_from_admin_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+proposals_gem_dir = Gem::Specification.find_by_name("decidim-proposals").gem_dir
+files = Dir.glob(File.join(proposals_gem_dir, "spec/shared/*.rb"))
+files.each { |f| require(f) }
 
 describe "Admin views proposal details from admin", type: :system do
   include_context "when admin manages proposals"
+  include_context "manage proposals"
   include ActionView::Helpers::TextHelper
 
   let(:participatory_process) { create(:participatory_process, :with_steps, organization: organization, scope: participatory_process_scope) }
