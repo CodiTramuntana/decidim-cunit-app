05cec2b1419bd8c24dda2bcdc707261f4701800f
Laura Jaime <laura.jv@coditramuntana.com>
2022-09-12 11:37:42 +0200

    Fix tests

--- spec/system/admin/view_proposal_details_from_admin_spec.rb
+++ spec/system/admin/view_proposal_details_from_admin_spec.rb
@@ -7,35 +7,28 @@ files.each { |f| require(f) }
 
 describe "Admin views proposal details from admin", type: :system do
   include_context "when admin manages proposals"
-  include_context "manage proposals"
   include ActionView::Helpers::TextHelper
 
-  let(:participatory_process) { create(:participatory_process, :with_steps, organization: organization, scope: participatory_process_scope) }
   let(:participatory_process_scope) { nil }
+  let(:participatory_process) { create(:participatory_process, :with_steps, organization: organization, scope: participatory_process_scope) }
 
-  it "has a link to the proposal" do
-    go_to_admin_proposal_page(proposal)
-    path = "processes/#{participatory_process.slug}/f/#{component.id}/proposals/#{proposal.id}"
-
-    expect(page).to have_selector("a", text: path)
+  before do
+    click_link proposal.title["en"]
   end
 
   describe "with personal data" do
     let!(:proposal) { create :proposal, component: current_component, author_name: "Beck", author_surname: "Ronald", author_phone: "650650650" }
 
     it "has a author name" do
-      go_to_admin_proposal_page(proposal)
-      expect(page).to have_content("Nom: #{proposal.author_name}")
+      expect(page).to have_content("Name: #{proposal.author_name}")
     end
 
     it "has a author surname" do
-      go_to_admin_proposal_page(proposal)
-      expect(page).to have_content("Cognom: #{proposal.author_surname}")
+      expect(page).to have_content("Surname: #{proposal.author_surname}")
     end
 
     it "has a author phone" do
-      go_to_admin_proposal_page(proposal)
-      expect(page).to have_content("Telèfon: #{proposal.author_phone}")
+      expect(page).to have_content("Phone: #{proposal.author_phone}")
     end
   end
 end
