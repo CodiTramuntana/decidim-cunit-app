341b91cdcc874e6d7bfa76de8b6b3e54669422a6
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2025-07-10 16:52:24 +0200

    Make decorators Zeitwerk compatible (#37)
    * Make decorators Zeitwerk compatible
    
    * Add Zeitwerk check GitHub Action
    
    * Update decidim-cdtb

--- app/decorators/commands/proposals/create_proposal_decorator.rb
+++ app/decorators/commands/proposals/create_proposal_decorator.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
-Decidim::Proposals::CreateProposal.class_eval do
-  def create_proposal
-    PaperTrail.request(enabled: false) do
-      @proposal = Decidim.traceability.perform_action!(
-        :create,
-        Decidim::Proposals::Proposal,
-        @current_user,
-        visibility: "public-only"
-      ) do
-        proposal = Decidim::Proposals::Proposal.new(
-          title: {
-            I18n.locale => title_with_hashtags
-          },
-          body: {
-            I18n.locale => body_with_hashtags
-          },
-          author_name: form.author_name,
-          author_surname: form.author_surname,
-          author_phone: form.author_phone,
-          component: form.component
-        )
-        proposal.add_coauthor(@current_user, user_group: user_group)
-        proposal.save!
-        proposal
+class Commands::Proposals::CreateProposalDecorator
+  def self.decorate
+    Decidim::Proposals::CreateProposal.class_eval do
+      def create_proposal
+        PaperTrail.request(enabled: false) do
+          @proposal = Decidim.traceability.perform_action!(
+            :create,
+            Decidim::Proposals::Proposal,
+            @current_user,
+            visibility: "public-only"
+          ) do
+            proposal = Decidim::Proposals::Proposal.new(
+              title: {
+                I18n.locale => title_with_hashtags
+              },
+              body: {
+                I18n.locale => body_with_hashtags
+              },
+              author_name: form.author_name,
+              author_surname: form.author_surname,
+              author_phone: form.author_phone,
+              component: form.component
+            )
+            proposal.add_coauthor(@current_user, user_group: user_group)
+            proposal.save!
+            proposal
+          end
+        end
       end
     end
   end
 end
+
+Commands::Proposals::CreateProposalDecorator.decorate
--- app/decorators/forms/proposals/proposal_wizard_create_step_form_decorator.rb
+++ app/decorators/forms/proposals/proposal_wizard_create_step_form_decorator.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
-Decidim::Proposals::ProposalWizardCreateStepForm.class_eval do
-  attribute :author_name, String
-  attribute :author_surname, String
-  attribute :author_phone, String
+class Forms::Proposals::ProposalWizardCreateStepFormDecorator
+  def self.decorate
+    Decidim::Proposals::ProposalWizardCreateStepForm.class_eval do
+      attribute :author_name, String
+      attribute :author_surname, String
+      attribute :author_phone, String
+    end
+  end
 end
+
+Forms::Proposals::ProposalWizardCreateStepFormDecorator.decorate
